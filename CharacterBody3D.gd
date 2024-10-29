2308fee4163a2994d4d592f187655bc60a1a7a95
Adam Côté <adam.cote.1@ulaval.ca>
2024-10-28 20:04:19 -0400

    Robot bouge et attaque

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,31 +1,61 @@
 extends CharacterBody3D
 
+@export var speed: float = 15.0
+@export var rotation_speed: float = 5.0
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@onready var animation = $robot/AnimationPlayer
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var cannot_move = false
+var just_stopped = false
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
-func _physics_process(delta):
-	# Add the gravity.
+func _process(delta: float) -> void:
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= gravity*delta
+	else :
+		velocity.y = 0
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var input_direction = Vector3(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		0,
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	# For the attack animation
+	if Input.is_action_just_pressed("space"):
+		cannot_move = true
+		animation.play("attackspinlonghands")
+		
+	if Input.is_action_just_pressed("h"):
+		cannot_move = true
+		animation.play("hello")
 
-	move_and_slide()
+	# For the movement
+	if input_direction != Vector3.ZERO:
+		velocity.x = input_direction.x * speed
+		velocity.z = input_direction.z * speed
+		var target_rotation = 0
+		if velocity.x < 0:
+			target_rotation = input_direction.angle_to(Vector3.FORWARD)
+		else:
+			target_rotation = -input_direction.angle_to(Vector3.FORWARD)
+		rotation.y = lerp_angle(rotation.y, target_rotation, rotation_speed * delta)
+		
+		if !animation.is_playing():
+			cannot_move = false
+			animation.play("walking")
+		if not cannot_move:
+			just_stopped = true
+
+	if not cannot_move:
+			move_and_slide()
+
+	if input_direction == Vector3.ZERO:
+		if not cannot_move:
+			if just_stopped:
+				animation.play("walkingstop")
+				just_stopped = false
+			elif !animation.is_playing():
+				animation.play("iddle")
+		velocity = Vector3(0, velocity.y, 0)  
+
