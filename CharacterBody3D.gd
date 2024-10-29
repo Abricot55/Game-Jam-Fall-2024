acac2e548b97a6288ba3dedf0d7519c0189cfb05
Adam Côté <adam.cote.1@ulaval.ca>
2024-10-28 22:28:15 -0400

    attaque marche, et on peut mettre des boulons dans le coffre

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -9,6 +9,13 @@ var cannot_move = false
 var just_stopped = false
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var number_scrap = 5
+var team = 0
+signal attack
+
+
+var ATTACK_RANGE = 5
+
 func _process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity*delta
@@ -25,6 +32,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("space"):
 		cannot_move = true
 		animation.play("attackspinlonghands")
+		emit_signal("attack")
 		
 	if Input.is_action_just_pressed("h"):
 		cannot_move = true
@@ -59,3 +67,34 @@ func _process(delta: float) -> void:
 				animation.play("iddle")
 		velocity = Vector3(0, velocity.y, 0)  
 
+func get_num_scrap():
+	return number_scrap
+
+func add_scrap():
+	number_scrap += 1
+	
+func remove_a_scrap():
+	number_scrap -= 1
+
+func attacked():
+	if number_scrap < 5:
+		number_scrap -= 2
+	elif number_scrap < 10:
+		number_scrap -= 3
+	elif number_scrap < 15:
+		number_scrap -= 4
+	elif number_scrap < 20:
+		number_scrap -= 5
+		
+func set_team(num : int):
+	team = num
+
+func get_team():
+	return team
+
+func in_attack_area(position_other : Vector3):
+	var distance = global_transform.origin.distance_to(position_other)
+	if distance <= ATTACK_RANGE:  # Check distance
+		return true
+	return false
+	
